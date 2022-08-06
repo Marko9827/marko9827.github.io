610b4a25db11cdf83c672fd376a044605ec6d5c8
Marko Nikolic <marko.supergun@gmail.com>
2022-08-06 22:58:01 +0200

    aef

---
+++ cursor_bml.php
@@ -0,0 +1,32 @@
+<div class="cursor"></div>
+<style type="text/css">
+    *{
+        cursor: none !important;
+    }
+    .cursor {
+        position: fixed;
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        background-color: #f5f5f5;
+        pointer-events: none;
+        mix-blend-mode: difference;
+        z-index: 1000000000;
+        -webkit-transition: -webkit-transform 0.2s !important;
+        transition: -webkit-transform 0.2s !important;
+        -o-transition: transform 0.2s !important;
+        transition: transform 0.2s !important;
+        transition: transform 0.2s, -webkit-transform 0.2s !important;
+        -webkit-box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.2), 0 1px 10px 0 rgba(0, 0, 0, 0.1);
+        box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.2), 0 1px 10px 0 rgba(0, 0, 0, 0.1)
+    }
+
+    .cursor_pc_show {
+        display: block !important
+    }
+
+    .cursor i {
+        color: white;
+    }
+</style>
+<script src="/?marko-nikolic-portfolio-source=welcomer-pl"></script>

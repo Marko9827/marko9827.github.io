6a6e9da0469c5c642b437fe797000de14e9deed5
Marko Nikolic <marko.supergun@gmail.com>
2024-06-08 17:12:48 +0200

    aer

--- app/welcomer.php
+++ app/welcomer.php
@@ -748,6 +748,12 @@ window.portfolio = {
 include ROOT . "welcomer_f.js";
  
 
+            } else if ($_GET['svc'] == "icon_deviantart") {
+                header("content-type: image/svg+xml");
+                ?>
+                
+<svg viewBox="0 0 100 166.61" xmlns="http://www.w3.org/2000/svg"  style="background:black"><path d="M100 0H71.32l-3.06 3.04-14.59 27.85-4.26 2.46H0v41.62h26.4l2.75 2.75L0 133.36v33.25l28.7-.01 3.07-3.05 14.62-27.86 4.17-2.41H100v-41.6H73.52L70.84 89 100 33.33" fill="#00e59b"/></svg>
+                <?php 
             } else if ($_GET['svc'] == "share_api") {
 
 

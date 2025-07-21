b6025536fddc2ef2b0956ec20580fc5a1b7bc15f
Marko Nikolic <marko.supergun@gmail.com>
2025-07-22 01:34:05 +0200

    aef

--- app/index.php
+++ app/index.php
@@ -1174,10 +1174,11 @@ filter: drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.4)) !important;
             header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
             header("Cache-Control: post-check=0, pre-check=0", false);
             header("Pragma: no-cache");
-            header('Content-Type: application/json');
+            
+            header("content-type: text/javascript");
 
             echo "window.portfolio = $r;";
-            header("content-type: text/javascript");
+           # header("content-type: text/javascript");
             echo "window.portfolio = $r; \n \n";
             if (strpos($_SERVER['HTTP_HOST'], ".localhost")) {
                 echo "window.portfolio.host = 'https://api.localhost';";

cd884dc5612f6196fec837cc64dad5e42502a0f0
Marko Nikolic <marko.supergun@gmail.com>
2025-07-22 01:34:31 +0200

    fx31

--- app/index.php
+++ app/index.php
@@ -1199,7 +1199,8 @@ filter: drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.4)) !important;
 
 
              
-            $r = $this->get_data([
+  
+  /*          $r = $this->get_data([
                 "url" => "https://api.markonikolic98.com/app&id=A03429468246&json=all",
                 "headers" => [
                     'Content-Type: application/json',
@@ -1207,7 +1208,7 @@ filter: drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.4)) !important;
                 ]
             ]); 
          echo "const portfolio = $r; \n"; 
-
+*/
 
             echo "window.stmp = '".base64_encode("$_SERVER[HTTP_HOST]")."';";
             # include ROOT . "welcomer_f_old.js";
